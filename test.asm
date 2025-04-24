2507d924e7421a46b53e0fde66824fd63630a6ed
hassan khan <117458014+hassan689@users.noreply.github.com>
2025-04-24 12:09:16 +0500

    Add files via upload

--- test.asm
+++ test.asm
@@ -12,8 +12,6 @@ mouth_state: db 0 ; 0=open, 1=closed
 speed_counter: db 0 
 random_seed: dw 0xABCD ; Seed for random number generator
 
-; Ghost data structures (4 ghosts)
-; Change ghost initialization to place them in the middle box
 ghosts:
     ; ghost 1
     dw 5, 20
@@ -821,3 +819,741 @@ done_print:
     pop ax
     ret
 	
+
+
+
+
+
+
+
+
+
+;+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+;	[org 0x0100]
+; jmp start
+
+; ; Game state variables
+; row: dw 10      ; initial row position of Pacman
+; col: dw 40      ; initial column position of Pacman
+; direction: db 0 ; 0=right, 1=left, 2=up, 3=down
+; mouth_state: db 0 ; 0=open, 1=closed
+; speed_counter: db 0 
+; random_seed: dw 0xABCD ; Seed for random number generator
+
+; ; Ghost data structures (4 ghosts)
+; ghosts:
+    ; ; ghost 1 (red)
+    ; dw 10, 36
+    ; db 0
+    ; db 0x4C
+    ; ; ghost 2 (pink)
+    ; dw 10, 38
+    ; db 1
+    ; db 0x4D
+    ; ; ghost 3 (cyan)
+    ; dw 10, 40
+    ; db 2
+    ; db 0x4B
+    ; ; ghost 4 (yellow)
+    ; dw 10, 42
+    ; db 3
+    ; db 0x4E
+
+; oldtimer: dd 0  ; space for saving old timer ISR
+
+; ; Maze definition (80x25)
+; ; 0 = empty, 1 = path, 2 = wall, 3 = dot, 4 = power pellet
+; map:
+; db 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2
+; db 2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2
+; db 2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,2
+; db 2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,2
+; db 2,1,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,2,1,2
+; db 2,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,2,1,2
+; db 2,1,2,1,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,2,1,2,1,2
+; db 2,1,2,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,2,1,2,1,2
+; db 2,1,2,1,2,1,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,2,1,2,1,2,1,2
+; db 2,1,2,1,2,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,2,1,2,1,2,1,2
+; db 2,1,2,1,2,1,2,1,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,2,1,2,1,2,1,2,1,2
+; db 2,1,1,1,2,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,2,1,1,1,2,1,2
+; db 2,2,2,1,2,1,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,2,1,2,2,2,1,2
+; db 2,1,1,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,2
+; db 2,1,2,2,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,2,2,2,2,2
+; db 2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2
+; db 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2
+
+; ;-------------------------------------------------------------------
+; ; Timer interrupt service routine (for automatic movement)
+; ;-------------------------------------------------------------------
+; timerisr:
+    ; push ax
+    ; push bx
+    ; push cx
+    ; push dx
+    ; push si
+    ; push di
+    ; push es
+    
+    ; ; Only move every 8 timer ticks (adjust for speed)
+    ; inc byte [speed_counter]
+    ; cmp byte [speed_counter], 2
+    ; jne skip_movement
+    
+    ; mov byte [speed_counter], 0
+    
+    ; ; ----------------------------
+    ; ; Move Pacman
+    ; ; ----------------------------
+    ; ; Save current position
+    ; mov bx, [row]
+    ; push bx
+    ; mov bx, [col]
+    ; push bx
+    
+    ; ; Move according to current direction
+    ; cmp byte [direction], 0
+    ; je move_right_auto
+    ; cmp byte [direction], 1
+    ; je move_left_auto
+    ; cmp byte [direction], 2
+    ; je move_up_auto
+    ; cmp byte [direction], 3
+    ; je move_down_auto
+    
+; move_right_auto:
+    ; inc word [col]
+    ; jmp check_pacman_move
+
+; move_left_auto:
+    ; dec word [col]
+    ; jmp check_pacman_move
+
+; move_up_auto:
+    ; dec word [row]
+    ; jmp check_pacman_move
+
+; move_down_auto:
+    ; inc word [row]
+    
+; check_pacman_move:
+    ; ; Check if new position is valid (path = 1)
+    ; call check_position_valid
+    ; jc invalid_pacman_move
+    
+    ; ; Position is valid
+    ; jmp update_pacman_position
+    
+; invalid_pacman_move:
+    ; ; Restore old position
+    ; pop word [col]
+    ; pop word [row]
+    ; push word [row]
+    ; push word [col]
+    
+    ; ; Choose a new random direction that is valid
+    ; call find_valid_direction
+    ; mov [direction], al
+    
+; update_pacman_position:
+    ; ; Toggle mouth state
+    ; xor byte [mouth_state], 1
+    
+    ; ; Erase at old position
+    ; pop bx          ; old col
+    ; pop ax          ; old row
+    ; push word [row] ; save new row
+    ; push word [col] ; save new col
+    ; mov [col], bx
+    ; mov [row], ax
+    ; call erase_pacman
+    ; pop word [col]  ; restore new col
+    ; pop word [row]  ; restore new row
+    
+    ; ; Draw at new position
+    ; call print_pacman
+    
+    ; ; ----------------------------
+    ; ; Move Ghosts
+    ; ; ----------------------------
+    ; mov si, ghosts
+    ; mov cx, 4
+    
+; ghost_loop:
+    ; push cx
+    ; push si
+    
+    ; ; Save current ghost position
+    ; mov ax, [si]
+    ; mov bx, [si+2]
+    ; push ax
+    ; push bx
+    
+    ; ; 25% chance to change direction randomly
+    ; call random_byte
+    ; and al, 0x03
+    ; cmp al, 0
+    ; jne no_dir_change
+    
+    ; ; Try to find a new valid direction
+    ; call find_valid_direction
+    ; mov [si+4], al     ; update ghost direction
+    
+; no_dir_change:
+    ; ; Move ghost according to its direction
+    ; mov al, [si+4]      ; direction
+    ; call try_ghost_move
+    ; jnc move_valid
+    
+    ; ; If move failed, try alternative directions
+    ; mov cx, 4           ; try 4 directions
+; try_again:
+    ; mov [si+4], cl      ; try direction 0-3
+    ; dec byte [si+4]     ; adjust to 0-3
+    ; call try_ghost_move
+    ; jnc move_valid
+    ; loop try_again
+    
+    ; ; If all directions fail, restore position
+    ; pop bx
+    ; pop ax
+    ; mov [si+2], bx
+    ; mov [si], ax
+    ; jmp next_ghost
+    
+; move_valid:
+    ; ; Complete the move
+    ; add sp, 4           ; clean stack
+    ; call erase_ghost
+    ; call print_ghost
+    
+; next_ghost:
+    ; pop si
+    ; pop cx
+    ; add si, 6
+    ; loop ghost_loop
+    
+; skip_movement:
+    ; pop es
+    ; pop di
+    ; pop si
+    ; pop dx
+    ; pop cx
+    ; pop bx
+    ; pop ax
+    ; jmp far [cs:oldtimer] ; chain to original timer ISR
+
+; ; Helper function to try moving ghost
+; try_ghost_move:
+    ; ; Input: AL = direction (0-3)
+    ; ; Returns CF=0 if move valid, CF=1 if invalid
+    ; mov ax, [si]
+    ; mov bx, [si+2]
+    
+    ; cmp al, 0
+    ; je .right
+    ; cmp al, 1
+    ; je .left
+    ; cmp al, 2
+    ; je .up
+    
+    ; ; down
+    ; inc ax
+    ; jmp .check
+; .right:
+    ; inc bx
+    ; jmp .check
+; .left:
+    ; dec bx
+    ; jmp .check
+; .up:
+    ; dec ax
+    
+; .check:
+    ; call check_position_valid
+    ; jc .invalid
+    ; mov [si], ax        ; update position
+    ; mov [si+2], bx
+    ; clc
+    ; ret
+; .invalid:
+    ; stc
+    ; ret
+
+; ;-------------------------------------------------------------------
+; ; Improved Random Functions
+; ;-------------------------------------------------------------------
+; random_byte:
+    ; push bx
+    ; push dx
+    
+    ; mov ax, [random_seed]
+    ; mov bx, ax
+    ; shl ax, 7
+    ; xor ax, bx
+    ; mov bx, ax
+    ; shr ax, 9
+    ; xor ax, bx
+    ; mov bx, ax
+    ; shl ax, 8
+    ; xor ax, bx
+    ; mov [random_seed], ax
+    
+    ; pop dx
+    ; pop bx
+    ; ret
+
+; ; Enhanced direction finding
+; find_valid_direction:
+    ; push cx
+    ; push si
+    
+    ; ; First try current direction
+    ; mov al, [si+4]
+    ; call test_direction
+    ; jnc .valid_found
+    
+    ; ; Try all directions systematically
+    ; mov cx, 4
+    ; mov al, 0
+    
+; .try_directions:
+    ; call test_direction
+    ; jnc .valid_found
+    ; inc al
+    ; and al, 3
+    ; loop .try_directions
+    
+    ; ; If all fail, return current direction
+    ; mov al, [si+4]
+    
+; .valid_found:
+    ; pop si
+    ; pop cx
+    ; ret
+
+; test_direction:
+    ; push ax
+    ; push bx
+    
+    ; mov bx, [si+2]
+    ; mov ax, [si]
+    
+    ; cmp al, 0
+    ; je .right
+    ; cmp al, 1
+    ; je .left
+    ; cmp al, 2
+    ; je .up
+    
+    ; ; down
+    ; inc ax
+    ; jmp .check
+; .right:
+    ; inc bx
+    ; jmp .check
+; .left:
+    ; dec bx
+    ; jmp .check
+; .up:
+    ; dec ax
+    
+; .check:
+    ; call check_position_valid
+    ; pop bx
+    ; pop ax
+    ; ret
+
+; ; Check if position (ax=row, bx=col) is valid
+; check_position_valid:
+    ; push ax
+    ; push bx
+    ; push si
+    
+    ; ; Boundary check
+    ; cmp ax, 0
+    ; jl .invalid
+    ; cmp ax, 24
+    ; jg .invalid
+    ; cmp bx, 0
+    ; jl .invalid
+    ; cmp bx, 79
+    ; jg .invalid
+    
+    ; ; Map check
+    ; mov si, ax
+    ; mov ax, 80
+    ; mul si
+    ; add ax, bx
+    ; lea si, [map]
+    ; add si, ax
+    ; cmp byte [si], 1   ; 1 is path
+    ; je .valid
+    ; cmp byte [si], 3   ; 3 is dot (also valid)
+    ; je .valid
+    ; cmp byte [si], 4   ; 4 is power pellet (valid)
+    ; je .valid
+    
+    ; jmp .invalid
+; .valid:
+    ; clc
+    ; jmp .done
+; .invalid:
+    ; stc
+; .done:
+    ; pop si
+    ; pop bx
+    ; pop ax
+    ; ret
+
+; ;-------------------------------------------------------------------
+; ; Drawing functions
+; ;-------------------------------------------------------------------
+; print_pacman:
+    ; push ax
+    ; push es
+    ; push di
+    
+    ; mov ax, 0xb800
+    ; mov es, ax
+    ; mov ax, 80
+    ; mul word [row]
+    ; add ax, [col]
+    ; shl ax, 1
+    ; mov di, ax
+    
+    ; cmp byte [mouth_state], 0
+    ; je .mouth_open
+    
+    ; ; Closed mouth
+    ; mov word [es:di], 0x0E0F ; yellow circle
+    ; jmp .done
+    
+; .mouth_open:
+    ; cmp byte [direction], 0
+    ; je .face_right
+    ; cmp byte [direction], 1
+    ; je .face_left
+    ; cmp byte [direction], 2
+    ; je .face_up
+    
+    ; ; Face down
+    ; mov word [es:di], 0x0E1E
+    ; jmp .done
+    
+; .face_right:
+    ; mov word [es:di], 0x0E3E
+    ; jmp .done
+    
+; .face_left:
+    ; mov word [es:di], 0x0E3C
+    ; jmp .done
+    
+; .face_up:
+    ; mov word [es:di], 0x0E1F
+    
+; .done:
+    ; pop di
+    ; pop es
+    ; pop ax
+    ; ret
+
+; erase_pacman:
+    ; push ax
+    ; push es
+    ; push di
+    
+    ; mov ax, 0xb800
+    ; mov es, ax
+    ; mov ax, 80
+    ; mul word [row]
+    ; add ax, [col]
+    ; shl ax, 1
+    ; mov di, ax
+    
+    ; ; Check what was originally there
+    ; mov ax, [row]
+    ; mov bx, [col]
+    ; call check_map_value
+    
+    ; ; Restore original map element
+    ; cmp al, 1
+    ; je .restore_path
+    ; cmp al, 2
+    ; je .restore_wall
+    ; cmp al, 3
+    ; je .restore_dot
+    ; cmp al, 4
+    ; je .restore_pellet
+    
+    ; ; Default to empty space
+    ; mov word [es:di], 0x0720
+    ; jmp .done
+    
+; .restore_path:
+    ; mov word [es:di], 0x0720 ; space
+    ; jmp .done
+    
+; .restore_wall:
+    ; mov word [es:di], 0x1900 | 0xDB ; blue wall
+    ; jmp .done
+    
+; .restore_dot:
+    ; mov word [es:di], 0x0700 | 0xFA ; small dot
+    ; jmp .done
+    
+; .restore_pellet:
+    ; mov word [es:di], 0x0700 | 0xF9 ; power pellet
+    
+; .done:
+    ; pop di
+    ; pop es
+    ; pop ax
+    ; ret
+
+; print_ghost:
+    ; push ax
+    ; push es
+    ; push di
+    
+    ; mov ax, 0xb800
+    ; mov es, ax
+    ; mov ax, 80
+    ; mul word [si]    ; row
+    ; add ax, [si+2]  ; col
+    ; shl ax, 1
+    ; mov di, ax
+    
+    ; mov al, 0x01    ; smiley face
+    ; mov ah, [si+5]  ; color
+    ; mov [es:di], ax
+    
+    ; pop di
+    ; pop es
+    ; pop ax
+    ; ret
+
+; erase_ghost:
+    ; push ax
+    ; push es
+    ; push di
+    
+    ; mov ax, 0xb800
+    ; mov es, ax
+    ; mov ax, 80
+    ; mul word [si]    ; row
+    ; add ax, [si+2]  ; col
+    ; shl ax, 1
+    ; mov di, ax
+    
+    ; ; Check what was originally there
+    ; mov ax, [si]    ; row
+    ; mov bx, [si+2]  ; col
+    ; call check_map_value
+    
+    ; ; Restore original map element
+    ; cmp al, 1
+    ; je .restore_path
+    ; cmp al, 2
+    ; je .restore_wall
+    ; cmp al, 3
+    ; je .restore_dot
+    ; cmp al, 4
+    ; je .restore_pellet
+    
+    ; ; Default to empty space
+    ; mov word [es:di], 0x0720
+    ; jmp .done
+    
+; .restore_path:
+    ; mov word [es:di], 0x0720 ; space
+    ; jmp .done
+    
+; .restore_wall:
+    ; mov word [es:di], 0x1900 | 0xDB ; blue wall
+    ; jmp .done
+    
+; .restore_dot:
+    ; mov word [es:di], 0x0700 | 0xFA ; small dot
+    ; jmp .done
+    
+; .restore_pellet:
+    ; mov word [es:di], 0x0700 | 0xF9 ; power pellet
+    
+; .done:
+    ; pop di
+    ; pop es
+    ; pop ax
+    ; ret
+
+; ; Get map value at position (ax=row, bx=col)
+; ; Returns al = map value
+; check_map_value:
+    ; push si
+    
+    ; ; offset = row * 80 + col
+    ; mov si, ax
+    ; mov ax, 80
+    ; mul si
+    ; add ax, bx
+    ; lea si, [map]
+    ; add si, ax
+    ; mov al, [si]
+    
+    ; pop si
+    ; ret
+
+; ;-------------------------------------------------------------------
+; ; Initialization and main loop
+; ;-------------------------------------------------------------------
+; start:
+    ; ; Initialize random seed
+    ; mov ah, 0x00
+    ; int 0x1A
+    ; mov [random_seed], dx
+    
+    ; ; Save old timer ISR
+    ; xor ax, ax
+    ; mov es, ax
+    ; mov ax, [es:8*4]
+    ; mov [oldtimer], ax
+    ; mov ax, [es:8*4+2]
+    ; mov [oldtimer+2], ax
+    
+    ; ; Install new timer ISR
+    ; cli
+    ; mov word [es:8*4], timerisr
+    ; mov [es:8*4+2], cs
+    ; sti
+    
+    ; ; Clear screen and draw map
+    ; call clear_screen
+    ; call draw_map
+    
+    ; ; Draw initial game elements
+    ; call print_pacman
+    
+    ; ; Initialize and draw ghosts
+    ; mov si, ghosts
+    ; mov cx, 4
+; init_ghosts:
+    ; call print_ghost
+    ; add si, 6
+    ; loop init_ghosts
+    
+    ; ; Terminate and stay resident
+    ; mov dx, start
+    ; add dx, 15
+    ; mov cl, 4
+    ; shr dx, cl
+    ; mov ax, 0x3100
+    ; int 0x21
+
+; ; Draw the complete map
+; draw_map:
+    ; push ax
+    ; push bx
+    ; push cx
+    ; push dx
+    ; push si
+    ; push di
+    ; push es
+    
+    ; mov ax, 0xb800
+    ; mov es, ax
+    ; xor si, si          ; row = 0
+    
+; row_loop:
+    ; cmp si, 25
+    ; jge done_drawing
+    
+    ; xor di, di          ; col = 0
+
+; col_loop:
+    ; cmp di, 80
+    ; jge next_row
+
+    ; ; Get map value
+    ; mov ax, si
+    ; mov bx, 80
+    ; mul bx
+    ; add ax, di
+    ; lea bx, [map]
+    ; add bx, ax
+    ; mov al, [bx]
+    
+    ; ; Calculate screen position
+    ; mov ax, si
+    ; mov bx, 80
+    ; mul bx
+    ; add ax, di
+    ; shl ax, 1
+    ; mov di, ax
+    
+    ; ; Draw appropriate character
+    ; cmp byte [bx], 1
+    ; je .path
+    ; cmp byte [bx], 2
+    ; je .wall
+    ; cmp byte [bx], 3
+    ; je .dot
+    ; cmp byte [bx], 4
+    ; je .pellet
+    
+    ; ; Empty space
+    ; mov word [es:di], 0x0720
+    ; jmp .next
+    
+; .path:
+    ; mov word [es:di], 0x0720 ; space
+    ; jmp .next
+    
+; .wall:
+    ; mov word [es:di], 0x1900 | 0xDB ; blue wall
+    ; jmp .next
+    
+; .dot:
+    ; mov word [es:di], 0x0700 | 0xFA ; small dot
+    ; jmp .next
+    
+; .pellet:
+    ; mov word [es:di], 0x0700 | 0xF9 ; power pellet
+    
+; .next:
+    ; shr di, 1          ; convert back to column
+    ; inc di
+    ; jmp col_loop
+
+; next_row:
+    ; inc si
+    ; jmp row_loop
+
+; done_drawing:
+    ; pop es
+    ; pop di
+    ; pop si
+    ; pop dx
+    ; pop cx
+    ; pop bx
+    ; pop ax
+    ; ret
+
+; clear_screen:
+    ; push ax
+    ; push es
+    ; push di
+    
+    ; mov ax, 0xb800
+    ; mov es, ax
+    ; xor di, di
+    ; mov ax, 0x0720
+    
+; .clear_loop:
+    ; mov [es:di], ax
+    ; add di, 2
+    ; cmp di, 4000
+    ; jl .clear_loop
+    
+    ; pop di 
+    ; pop es
+    ; pop ax
+    ; ret
